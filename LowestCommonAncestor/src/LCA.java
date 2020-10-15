fde247a0e224cba08287ec3c98eb97a3aa72037b
miocalla <61199986+miocalla@users.noreply.github.com>
2020-10-15 15:34:54 +0100

    Update LCA.java

--- LowestCommonAncestor/src/LCA.java
+++ LowestCommonAncestor/src/LCA.java
@@ -15,44 +15,40 @@ class Node
   
 public class LCA 
 { 
-    //Root of the Binary Tree 
     Node root; 
   
-    Node findLCA(int n1, int n2) 
+    Node findLCA(int number1, int number2) 
     { 
-        return findLCA(root, n1, n2); 
+        return findLCA(root, number1, number2); 
     } 
   
-    // This function returns pointer to LCA of two given 
-    // values n1 and n2. This function assumes that n1 and 
-    // n2 are present in Binary Tree 
-    Node findLCA(Node node, int n1, int n2) 
+    // Returns pointer to LCA of two given values number1 and number2, assuming they are present in Binary Tree
+    Node findLCA(Node node, int number1, int number2) 
     { 
-        // Base case 
         if (node == null) 
-            return null; 
+        {
+        	return null; 
+        } 
   
-        // If either n1 or n2 matches with root's key, report 
-        // the presence by returning root (Note that if a key is 
-        // ancestor of other, then the ancestor key becomes LCA 
-        if (node.data == n1 || node.data == n2) 
-            return node; 
-  
+        // If  number1 or number2 matches with root's key, return root 
+        if (node.data == number1 || node.data == number2) 
+        {
+        	return node; 
+        }  
         // Look for keys in left and right subtrees 
-        Node left_lca = findLCA(node.left, n1, n2); 
-        Node right_lca = findLCA(node.right, n1, n2); 
+        Node left_lca = findLCA(node.left, number1, number2); 
+        Node right_lca = findLCA(node.right, number1, number2); 
   
-        // If both of the above calls return Non-NULL, then one key 
-        // is present in once subtree and other is present in other, 
-        // So this node is the LCA 
+        // If both of the above calls return Non-NULL, then one key is present in once subtree and other is present in other, meaning node is LCA
         if (left_lca!=null && right_lca!=null) 
-            return node; 
-  
+        {
+        	return node; 
+        }
         // Otherwise check if left subtree or right subtree is LCA 
         return (left_lca != null) ? left_lca : right_lca; 
     } 
   
-    /* Driver program to test above functions */
+
     public static void main(String args[]) 
     { 
         LCA tree = new LCA(); 
@@ -63,13 +59,10 @@ public class LCA
         tree.root.left.right = new Node(5); 
         tree.root.right.left = new Node(6); 
         tree.root.right.right = new Node(7); 
-        System.out.println("LCA(4, 5) = " + 
-                            tree.findLCA(4, 5).data); 
-        System.out.println("LCA(4, 6) = " + 
-                            tree.findLCA(4, 6).data); 
-        System.out.println("LCA(3, 4) = " + 
-                            tree.findLCA(3, 4).data); 
-        System.out.println("LCA(2, 4) = " + 
-                            tree.findLCA(2, 4).data); 
+        
+        System.out.println("LCA(4, 5) = " + tree.findLCA(4, 5).data); 
+        System.out.println("LCA(4, 6) = " + tree.findLCA(4, 6).data); 
+        System.out.println("LCA(3, 4) = " + tree.findLCA(3, 4).data); 
+        System.out.println("LCA(2, 4) = " + tree.findLCA(2, 4).data); 
     } 
 } 
