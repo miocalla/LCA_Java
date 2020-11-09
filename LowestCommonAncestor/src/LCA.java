1704be99c38fc29656a60eedb46dcde85c4a27c8
miocalla <61199986+miocalla@users.noreply.github.com>
2020-11-09 22:38:07 +0000

    Added DAG constructor

--- LowestCommonAncestor/src/LCA.java
+++ LowestCommonAncestor/src/LCA.java
@@ -23,6 +23,15 @@ public class LCA <Key extends Comparable<Key>, Value>{
 		}
 
 	}
+	
+	class DAG 
+	{
+		private Node root;
+		public DAG(Node root)
+		{
+			this.root = root;
+		}
+	}
 
 	public boolean isEmpty()
 	{
