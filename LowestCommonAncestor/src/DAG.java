75cc9ba3ea59496a28077f038f1edeaa189c9c8f
miocalla <61199986+miocalla@users.noreply.github.com>
2020-11-09 22:15:57 +0000

    Rough code for DAG class
    Needs work

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -1,4 +1,16 @@
 
-public class DAG {
-
+public class DAG <Value> {
+	
+	private class Node {
+		
+		private Value val;
+		private Node[] successors;
+		
+		public Node(Value val) {
+			this.val = val;
+		}
+		
+	}
+	
+	private Node[] nodeList = new DAG.Node[0];
 }
