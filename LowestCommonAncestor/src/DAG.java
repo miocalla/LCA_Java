160db3c51f60b314f122e129c6faf4c89fae3b4f
miocalla <61199986+miocalla@users.noreply.github.com>
2020-11-09 22:33:26 +0000

    Updated DAG class
    Added methods to check if DAG is empty and if node n is in the graph

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -13,4 +13,34 @@ public class DAG <Value> {
 	}
 	
 	private Node[] nodeList = new DAG.Node[0];
+	
+	private int size() {
+		return nodeList.length;
+	}
+	
+	public boolean isEmpty() {
+		
+		if (size()==0) {
+			
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean contains(Value val)
+	{
+		boolean valueInList = false;
+		for (int i = 0; i < nodeList.length; i++)
+		{
+			if(nodeList[i].val == val)
+			{
+				valueInList = true;
+				break;
+			}
+		}
+		return valueInList;
+	}
 }
