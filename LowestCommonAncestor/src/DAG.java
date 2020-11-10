e93b295f0ade92c5caacbbf4b4c9eb2425d841f4
miocalla <61199986+miocalla@users.noreply.github.com>
2020-11-10 00:11:27 +0000

    Updated DAG class
    Removed code for different implementation and started new way

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -1,27 +1,48 @@
+import java.util.ArrayList; 
+import java.util.Iterator; 
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class DAG <Value> {
-	
-	private class Node {
-		
-		private Value val;
-		private Node[] successors;
-		
-		public Node(Value val) {
-			this.val = val;
-		}
-		
+public class DAG {
+
+	private int V;           
+	private int E;              
+	private ArrayList<Integer>[] adj;    
+	private int[] indegree;        
+	private boolean marked[];		
+	private boolean hasCycle;		
+	private boolean stack[];		
+	private int[] distanceTo;      
+	private int[] edgeTo;     
+
+
+	public DAG(int V)
+	{
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+		}              
 	}
-	
-	private Node[] nodeList = new DAG.Node[0];
-	
-	private int size() {
-		return nodeList.length;
+
+	//Returns current vertex
+	public int vertex() {
+		return V;	
 	}
-	
+
+	public int edge() {
+		return E;
+	}
+
 	public boolean isEmpty() {
-		
+
 		if (size()==0) {
-			
+
 			return true;
 		}
 		else
@@ -29,18 +50,5 @@ public class DAG <Value> {
 			return false;
 		}
 	}
-	
-	public boolean contains(Value val)
-	{
-		boolean valueInList = false;
-		for (int i = 0; i < nodeList.length; i++)
-		{
-			if(nodeList[i].val == val)
-			{
-				valueInList = true;
-				break;
-			}
-		}
-		return valueInList;
-	}
+
 }
